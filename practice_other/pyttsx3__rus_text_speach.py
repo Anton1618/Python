be59a0e29d63b791ce1b993164919f4d8c6ebba8
anton <akramskiy@yandex.ru>
2023-04-27 22:29:22 +0300

    Изучение возможностей библиотеки аудирования текста

---
+++ practice_other/pyttsx3__rus_text_speach.py
@@ -0,0 +1,12 @@
+'''pyttsx3 - библиотека преобразования текста в речь на Python
+
+'''
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)  # Скорость речи
+engine.setProperty('voice', 'ru')  # Русский язык
+
+text = input('Введите текст для произнесения: ')
+engine.say(text)
+engine.runAndWait()
