e1e50db24c86a8ad44260c95a0c557d73b5d42af
Anton <akramskiy@yandex.ru>
2024-04-20 01:09:39 +0300

    Множественные изменения в директории module_SQLite

--- module_SQLite/aggregationAndGrouping_players.py
+++ module_SQLite/aggregationAndGrouping_players.py
@@ -8,16 +8,15 @@
 '''
 
 import sqlite3 as sq
-from practice_functions.staff_functions import pretty_header, text_ellipsis
 
-db_path = 'module_SQLite/game_matches.db'
-def separator(sep='\n\n'):
-    print(sep)
+DB_PATH = 'module_SQLite/game_matches.db'
 
 
-text_ellipsis('Очистка БД предыдущей сессии')
-text_ellipsis('Сборка БД')
-with sq.connect(db_path) as con:
+
+
+print('Очистка БД предыдущей сессии...')
+print('Сборка новой БД...')
+with sq.connect(DB_PATH) as con:
     cursor = con.cursor()
     cursor.executescript('''
                         DROP TABLE IF EXISTS games;
@@ -41,13 +40,14 @@ with sq.connect(db_path) as con:
                             ('18.02.2024', 'DreamLeague Season 23', 'Dota 2', 100000, '9Pandas', 1),
                             ('10.02.2024', 'DreamLeague Season 22', 'Dota 2', 75000, 'NAVI', 1);
                         ''')
-separator()
+print('\n' * 4)
 
 
 
 
-pretty_header('Получение записей о призовых фондах для каждой игры, отсортированных по возрастанию размера фонда')
-with sq.connect(db_path) as con:
+print('===========================================================================')
+print('Получение записей о призовых фондах для каждой игры, отсортированных по возрастанию размера фонда')
+with sq.connect(DB_PATH) as con:
     cursor = con.cursor()
     cursor.execute('''
                     SELECT game, prize_fund
@@ -58,13 +58,14 @@ with sq.connect(db_path) as con:
 data = cursor.fetchall()
 for d in data:
     print(*d, sep=': ')
-separator()
+print('\n' * 4)
 
 
 
 
-pretty_header('Получение числа дисциплин и суммы призового фонда по каждой из них, с группировкой по размеру суммы фонда по убыванию')
-with sq.connect(db_path) as con:
+print('===========================================================================')
+print('Получение числа дисциплин и суммы призового фонда по каждой из них, с группировкой по размеру суммы призового фонда в убывающем порядке')
+with sq.connect(DB_PATH) as con:
     cursor = con.cursor()
     cursor.execute('''
                     SELECT game, count(game) AS count, SUM(prize_fund) AS sum
@@ -76,13 +77,14 @@ with sq.connect(db_path) as con:
 data = cursor.fetchall()
 for d in data:
     print(f'{d[0]} ({d[1]}): {d[2]}')
-separator()
+print('\n' * 4)
 
 
 
 
-pretty_header('Группировка игроков, где каждая группа включает общую сумму призового фонда и среднее значение по нему, а также сортировку по его значению по убыванию')
-with sq.connect(db_path) as con:
+print('===========================================================================')
+print('Группировка игроков, где каждая группа включает общую сумму призового фонда и среднее значение по нему, а также сортировку по его значению в убывающем порядке')
+with sq.connect(DB_PATH) as con:
     cursor = con.cursor()
     cursor.execute('''
                     SELECT players, ROUND(AVG(prize_fund)) AS avg, SUM(prize_fund) AS result
@@ -97,4 +99,4 @@ print("%10s   %10s   %10s ▼"%(*columns,))
 print(40 * '-')
 for num, d in enumerate(data):
     print("%10s | %10d | %10d"%(d[0], d[1], d[2]))
-separator()
+print('\n' * 4)
